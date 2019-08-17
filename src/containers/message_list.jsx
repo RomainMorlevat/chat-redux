8bd8901175a0980145a28defe1b3c3fa13f54321
Romain Morlevat <romainmorlevat@gmail.com>
2019-08-17 11:54:28 +0200

    Update messages every X seconds

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -8,7 +8,25 @@ import Message from '../components/message';
 import MessageForm from './message_form';
 
 class MessageList extends Component {
+  constructor(props) {
+    super(props);
+
+    let interval;
+  }
+
   componentWillMount() {
+    this.fetchMessagesFunction();
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.fetchMessagesFunction, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  fetchMessagesFunction = () => {
     this.props.fetchMessages(this.props.selectedChannel);
   }
 
