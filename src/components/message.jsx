fd73b7d12b99de92926703a2589940ae6f74cf58
Romain Morlevat <romainmorlevat@gmail.com>
2019-08-21 10:42:29 +0200

    Format datetime

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -5,7 +5,7 @@ const Message = (props) => {
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{props.message.author}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{props.message.created_at}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">{new Date(props.message.created_at).toLocaleTimeString()}</h6>
         <p className="card-text">{props.message.content}</p>
       </div>
     </div>
